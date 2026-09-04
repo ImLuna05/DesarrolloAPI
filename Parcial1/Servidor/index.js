c4f6c822fe5cf55c2048d600c8068f9775922541
ImLuna05 <alberto.rdzluna1109@gmail.com>
2026-09-04 17:54:46 -0500

    Update index.js

--- Parcial1/Servidor/index.js
+++ Parcial1/Servidor/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const recursoRouter = require('./router/recursoRouter.js');
+
 const app = express();
 const PORT = 3000;
+
+//Middleware de terceros
+app.use(cors());           // Permite peticiones desde otros dominios/origenes
+app.use(morgan('dev'));   // Registra en consola cada petición (método, ruta, status, tiempo)
+
+// ===== MIDDLEWARE DE APLICACIÓN (propio) =====
+// Función personalizada que se ejecuta antes de llegar a las rutas
+
+app.use((req,res,next) => {
+  const fecha = new Date().toISOString();
+  console.log('[Middleware propio] ' + fecha + ' - ' + req.method + ' ' + req.url);
+  next();
+});
 
 //Middleware para poder leer JSON en el body de las peticiones (POST, PUT)
 
@@ -9,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 // Ruta principal
-app.get('/', (req, res) => {
+app.get('/' , (req, res) => {
   res.send('Servidor Express funcionando correctamente (si jalo) :D');
 });
 
